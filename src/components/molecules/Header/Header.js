badff5cb1792b969419cc0876b02da3aa70b01e1
poisk <poiskov2016@gmail.com>
2021-10-05 11:36:14 +0300

    Authorization with password reset

--- src/components/molecules/Header/Header.js
+++ src/components/molecules/Header/Header.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import "./Header.css"
-import {Container, Nav, Navbar, NavDropdown, NavLink} from "react-bootstrap";
+import {Container, Nav, Navbar} from "react-bootstrap";
 import app from "../../../firebase_config/firebase";
 
 const Header = () => {
   return (
-    <Navbar expand="lg" className="head">
+    <Navbar expand="sm" className="head">
       <Container>
+        <Navbar.Brand href="/" className="name">
+          Всеобуч<br/>
+          Центр Инвест
 
-        <Navbar.Brand href="/" className="name">Всеобуч<br/>Центр Инвест</Navbar.Brand>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav>
             <Nav.Link href="/courses" className="title">Курсы</Nav.Link>
             <Nav.Link href="/about" className="title">О проекте</Nav.Link>
             <Nav.Link href="/feedbacks" className="title">Отзывы</Nav.Link>
