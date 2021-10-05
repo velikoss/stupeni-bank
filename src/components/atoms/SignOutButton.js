c655272c78ec3c091ec06a0ac6d50d7d7232cd32
Engozdev <denacua5@gmail.com>
2021-10-05 14:31:01 +0300

    Header

--- src/components/atoms/SignOutButton.js
+++ src/components/atoms/SignOutButton.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import app from "../../firebase_config/firebase";
-import {Link} from "react-router-dom";
 
 function SignOutButton() {
   return (
     <div>
-      <Link to="/login"><img src="../../img/Vector.svg" alt="Регайся, быстро!"/></Link>
       <button onClick={() => app.auth().signOut()}>Выйти</button>
     </div>
   );
--- src/components/molecules/Header/Header.js
+++ src/components/molecules/Header/Header.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import "./Header.css"
-import {Container, Nav, Navbar, NavDropdown, NavLink} from "react-bootstrap";
+import {Container, Nav, Navbar, NavLink} from "react-bootstrap";
 import app from "../../../firebase_config/firebase";
+import logo from "../../../img/logo.png"
 
 const Header = () => {
   return (
     <Navbar expand="lg" className="head">
       <Container>
-
+        <Nav>
+          <Nav.Link>
+            <img src={logo} width="70" height="85 " alt="Q"/>
+          </Nav.Link>
+        </Nav>
         <Navbar.Brand href="/" className="name">Всеобуч<br/>Центр Инвест</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/courses" className="title">Курсы</Nav.Link>
-            <Nav.Link href="/about" className="title">О проекте</Nav.Link>
+            <Nav.Link href="/about" className="title">Информация</Nav.Link>
             <Nav.Link href="/feedbacks" className="title">Отзывы</Nav.Link>
-            <Nav.Link onClick={() => app.auth().signOut()} className="title">Выйти</Nav.Link>
+          </Nav>
+          <Nav className="justify-content-end">
+            <NavLink onClick={() => app.auth().signOut()} className="title margin:0;">Выйти</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
