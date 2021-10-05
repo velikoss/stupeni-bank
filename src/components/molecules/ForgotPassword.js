f891183577d5c6069326b3b2cd52db62c53e4b00
CervaCrya <cerva1706@gmail.com>
2021-10-05 09:43:57 +0300

    RegLogAndForgotPage

--- src/components/molecules/ForgotPassword.js
+++ src/components/molecules/ForgotPassword.js
@@ -2,6 +2,7 @@ import React, {useRef, useState} from "react"
 import {Alert, Button, Card, Form} from "react-bootstrap"
 import {useAuth} from "./AuthContext";
 import {Link} from "react-router-dom"
+import './ForgotPassword.css'
 
 export default function ForgotPassword() {
   const emailRef = useRef()
@@ -28,27 +29,29 @@ export default function ForgotPassword() {
 
   return (
     <>
-      <Card>
+    <div id='ForgotPage'>
+      <Card id='ForgotCard'>
         <Card.Body>
-          <h2 className="text-center mb-4">Password Reset</h2>
+          <h2 className="text-center mb-4"><p>Восстановление</p> <p>пароля</p></h2>
           {error && <Alert variant="danger">{error}</Alert>}
           {message && <Alert variant="success">{message}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
+              <Form.Label>Почта</Form.Label>
               <Form.Control type="email" ref={emailRef} required/>
             </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Reset Password
+            <Button disabled={loading} id='ForgotButton' type="submit">
+              Продолжить
             </Button>
           </Form>
-          <div className="w-100 text-center mt-3">
-            <Link to="/login">Login</Link>
+          <div className="text-center mt-3">
+            <Link to="/login">Войти</Link>
           </div>
+          <div className="text-center mt-2">
+        Ещё нет аккаунта? <Link to="/signup">Зарегестрируйтесь</Link>
+      </div>
         </Card.Body>
       </Card>
-      <div className="w-100 text-center mt-2">
-        Need an account? <Link to="/signup">Sign Up</Link>
       </div>
     </>
   )
