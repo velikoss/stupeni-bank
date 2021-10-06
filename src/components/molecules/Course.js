0462a2d3ee33ac6fc3a6286a53660f1ee786e41d
Velikiy Kirill <50528703+velikoss@users.noreply.github.com>
2021-10-06 20:01:17 +0300

    Course.js

--- src/components/molecules/Course.js
+++ src/components/molecules/Course.js
@@ -23,47 +23,68 @@ const Course = () => {
                         <MenuItem>Тест</MenuItem>
                     </SubMenu>
                     <SubMenu title={`Отличия малого и большого бизнеса`}>
-                        <MenuItem>Лекция</MenuItem>
+                        <MenuItem>Лекция
+                            <Link to="/lesson2" />
+                        </MenuItem>
                         <MenuItem>Тест</MenuItem>
                     </SubMenu>
                     <SubMenu title={`Формула успеха малого бизнеса`}>
-                        <MenuItem>Лекция</MenuItem>
+                        <MenuItem>Лекция
+                            <Link to="/lesson3" />
+                        </MenuItem>
                         <MenuItem>Тест</MenuItem>
                     </SubMenu>
                     <SubMenu title={`Стратегия развития малого бизнеса`}>
-                        <MenuItem>Лекция</MenuItem>
+                        <MenuItem>Лекция
+                            <Link to="/lesson4" />
+                        </MenuItem>
                         <MenuItem>Тест</MenuItem>
                     </SubMenu>
                     <SubMenu title={`Управление малым и средним предприятием`}>
-                        <MenuItem>Лекция</MenuItem>
+                        <MenuItem>Лекция
+                            <Link to="/lesson5" />
+                        </MenuItem>
                         <MenuItem>Тест</MenuItem>
                     </SubMenu>
                     <SubMenu title={`Бизнес-план малого предприятия`}>
-                        <MenuItem>Лекция</MenuItem>
+                        <MenuItem>Лекция
+                            <Link to="/lesson6" /></MenuItem>
                         <MenuItem>Тест</MenuItem>
                     </SubMenu>
                     <SubMenu title={`Инвестиции и инновации`}>
-                        <MenuItem>Лекция</MenuItem>
+                        <MenuItem>Лекция
+                            <Link to="/lesson6" />
+                        </MenuItem>
                         <MenuItem>Тест</MenuItem>
                     </SubMenu>
                     <SubMenu title={`Особенности внешнеэкономической деятельности`}>
-                        <MenuItem>Лекция</MenuItem>
+                    <MenuItem>Лекция
+                            <Link to="/lesson6" />
+                        </MenuItem>
                         <MenuItem>Тест</MenuItem>
                     </SubMenu>
                     <SubMenu title={`Государство и малый бизнес`}>
-                        <MenuItem>Лекция</MenuItem>
+                    <MenuItem>Лекция
+                            <Link to="/lesson6" />
+                        </MenuItem>
                         <MenuItem>Тест</MenuItem>
                     </SubMenu>
                     <SubMenu title={`Налоги для малого бизнеса`}>
-                        <MenuItem>Лекция</MenuItem>
+                    <MenuItem>Лекция
+                            <Link to="/lesson6" />
+                        </MenuItem>
                         <MenuItem>Тест</MenuItem>
                     </SubMenu>
                     <SubMenu title={`Поддержка и развитие малого бизнеса Дона`}>
-                        <MenuItem>Лекция</MenuItem>
+                    <MenuItem>Лекция
+                            <Link to="/lesson6" />
+                        </MenuItem>
                         <MenuItem>Тест</MenuItem>
                     </SubMenu>
                     <SubMenu title={`Финансы для бизнеса`}>
-                        <MenuItem>Лекция</MenuItem>
+                    <MenuItem>Лекция
+                            <Link to="/lesson6" />
+                        </MenuItem>
                         <MenuItem>Тест</MenuItem>
                     </SubMenu>
                     <MenuItem>Экзамен</MenuItem>
