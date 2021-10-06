6d4654a5dc9a87b600841985c0123ef87062bb7e
poisk <poiskov2016@gmail.com>
2021-10-06 12:31:51 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import SignUp from "./components/molecules/SignUp";
 import SignOutButton from "./components/atoms/SignOutButton";
 import ForgotPassword from "./components/molecules/ForgotPassword";
 import Main from "./components/molecules/Main";
+import Courses from "./components/molecules/Courses";
 
 function App() {
   return (
     <div>
-      <PrivateRoute exact path="/" component={Main}/>
+      <Route exact path="/" component={Main}/>
       <Route exact path="/login" component={Login}/>
       <Route exact path="/signup" component={SignUp}/>
       <Route exact path="/forgot-password" component={ForgotPassword}/>
+      <PrivateRoute exact path="course" component={Courses}/>
     </div>
   );
 }
--- src/components/molecules/Courses.js
+++ src/components/molecules/Courses.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 const Courses = () => {
   return (
     <div>
-        <Link to=""></Link>
+
     </div>
   );
 };
