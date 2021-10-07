cba6e67b8211e09246d4eb4aee4b2406afb76160
CervaCrya <cerva1706@gmail.com>
2021-10-07 21:28:59 +0300

    TestSideBar

--- src/components/molecules/Course.js
+++ src/components/molecules/Course.js
@@ -1,80 +1,13 @@
 import React from 'react';
 import "./Course.css"
-import {Menu, MenuItem, ProSidebar, SidebarContent, SidebarHeader, SubMenu} from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
 import {Link} from "react-router-dom";
-
+import Sidebar from "./Sidebar"
 const Course = () => {
     return (
         <div id="CoursePage">
             <div id='Course'>
-            <ProSidebar>
-            <SidebarHeader>
-                Курс «Малый бизнес»
-            </SidebarHeader>
-            <SidebarContent>
-                <Menu>
-                    <SubMenu title={`Что такое малый бизнеc`}>
-                        <MenuItem>
-                            Лекция
-                            <Link to="/course/lection1" />
-                        </MenuItem>
-                        <MenuItem>
-                            Тест
-                            <Link to="/course/test1" />
-                        </MenuItem>
-                    </SubMenu>
-                    <SubMenu title={`Отличия малого и большого бизнеса`}>
-                        <MenuItem>
-                            Лекция
-                            <Link to="/course/lection2" />
-                        </MenuItem>
-                        <MenuItem>Тест</MenuItem>
-                    </SubMenu>
-                    <SubMenu title={`Формула успеха малого бизнеса`}>
-                        <MenuItem>Лекция</MenuItem>
-                        <MenuItem>Тест</MenuItem>
-                    </SubMenu>
-                    <SubMenu title={`Стратегия развития малого бизнеса`}>
-                        <MenuItem>Лекция</MenuItem>
-                        <MenuItem>Тест</MenuItem>
-                    </SubMenu>
-                    <SubMenu title={`Управление малым и средним предприятием`}>
-                        <MenuItem>Лекция</MenuItem>
-                        <MenuItem>Тест</MenuItem>
-                    </SubMenu>
-                    <SubMenu title={`Бизнес-план малого предприятия`}>
-                        <MenuItem>Лекция</MenuItem>
-                        <MenuItem>Тест</MenuItem>
-                    </SubMenu>
-                    <SubMenu title={`Инвестиции и инновации`}>
-                        <MenuItem>Лекция</MenuItem>
-                        <MenuItem>Тест</MenuItem>
-                    </SubMenu>
-                    <SubMenu title={`Особенности внешнеэкономической деятельности`}>
-                        <MenuItem>Лекция</MenuItem>
-                        <MenuItem>Тест</MenuItem>
-                    </SubMenu>
-                    <SubMenu title={`Государство и малый бизнес`}>
-                        <MenuItem>Лекция</MenuItem>
-                        <MenuItem>Тест</MenuItem>
-                    </SubMenu>
-                    <SubMenu title={`Налоги для малого бизнеса`}>
-                        <MenuItem>Лекция</MenuItem>
-                        <MenuItem>Тест</MenuItem>
-                    </SubMenu>
-                    <SubMenu title={`Поддержка и развитие малого бизнеса Дона`}>
-                        <MenuItem>Лекция</MenuItem>
-                        <MenuItem>Тест</MenuItem>
-                    </SubMenu>
-                    <SubMenu title={`Финансы для бизнеса`}>
-                        <MenuItem>Лекция</MenuItem>
-                        <MenuItem>Тест</MenuItem>
-                    </SubMenu>
-                    <MenuItem>Экзамен</MenuItem>
-                </Menu>
-            </SidebarContent>
-            </ProSidebar>
+                <Sidebar/>
             </div>
         </div>
     );
---
+++ src/components/molecules/Sidebar.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {Menu, MenuItem, ProSidebar, SidebarContent, SidebarHeader, SubMenu} from 'react-pro-sidebar';
+import {Link, useHistory} from "react-router-dom"
+const Sidebar = () => {
+    return (
+        <div>
+            <ProSidebar>
+            <SidebarHeader>
+                Курс «Малый бизнес»
+            </SidebarHeader>
+            <SidebarContent>
+                <Menu>
+                    <SubMenu title={`Что такое малый бизнеc`}>
+                        <MenuItem>
+                            Лекция
+                            <Link to="/course/lection1" />
+                        </MenuItem>
+                        <MenuItem>
+                            Тест
+                            <Link to="/course/test1" />
+                        </MenuItem>
+                    </SubMenu>
+                    <SubMenu title={`Отличия малого и большого бизнеса`}>
+                        <MenuItem>
+                            Лекция
+                            <Link to="/course/lection2" />
+                        </MenuItem>
+                        <MenuItem>Тест</MenuItem>
+                    </SubMenu>
+                    <SubMenu title={`Формула успеха малого бизнеса`}>
+                        <MenuItem>Лекция</MenuItem>
+                        <MenuItem>Тест</MenuItem>
+                    </SubMenu>
+                    <SubMenu title={`Стратегия развития малого бизнеса`}>
+                        <MenuItem>Лекция</MenuItem>
+                        <MenuItem>Тест</MenuItem>
+                    </SubMenu>
+                    <SubMenu title={`Управление малым и средним предприятием`}>
+                        <MenuItem>Лекция</MenuItem>
+                        <MenuItem>Тест</MenuItem>
+                    </SubMenu>
+                    <SubMenu title={`Бизнес-план малого предприятия`}>
+                        <MenuItem>Лекция</MenuItem>
+                        <MenuItem>Тест</MenuItem>
+                    </SubMenu>
+                    <SubMenu title={`Инвестиции и инновации`}>
+                        <MenuItem>Лекция</MenuItem>
+                        <MenuItem>Тест</MenuItem>
+                    </SubMenu>
+                    <SubMenu title={`Особенности внешнеэкономической деятельности`}>
+                        <MenuItem>Лекция</MenuItem>
+                        <MenuItem>Тест</MenuItem>
+                    </SubMenu>
+                    <SubMenu title={`Государство и малый бизнес`}>
+                        <MenuItem>Лекция</MenuItem>
+                        <MenuItem>Тест</MenuItem>
+                    </SubMenu>
+                    <SubMenu title={`Налоги для малого бизнеса`}>
+                        <MenuItem>Лекция</MenuItem>
+                        <MenuItem>Тест</MenuItem>
+                    </SubMenu>
+                    <SubMenu title={`Поддержка и развитие малого бизнеса Дона`}>
+                        <MenuItem>Лекция</MenuItem>
+                        <MenuItem>Тест</MenuItem>
+                    </SubMenu>
+                    <SubMenu title={`Финансы для бизнеса`}>
+                        <MenuItem>Лекция</MenuItem>
+                        <MenuItem>Тест</MenuItem>
+                    </SubMenu>
+                    <MenuItem>Экзамен</MenuItem>
+                </Menu>
+            </SidebarContent>
+            </ProSidebar>
+        </div>
+    );
+};
+
+export default Sidebar;
